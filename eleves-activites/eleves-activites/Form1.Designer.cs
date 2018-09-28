ff3259fdc324b2ab9d07db2039691da33e64e556
joris <joris.decoppet@cpnv.ch>
2018-09-28 09:33:55 +0200

    Ouvrir un CSV et l affiche dans une TBox

--- eleves-activites/eleves-activites/Form1.Designer.cs
+++ eleves-activites/eleves-activites/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace eleves_activites
 {
-    partial class Form1
+    partial class EleveActivites
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnOpenCSV = new System.Windows.Forms.Button();
+            this.OvrirCSV = new System.Windows.Forms.OpenFileDialog();
+            this.TxtBTes = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // BtnOpenCSV
+            // 
+            this.BtnOpenCSV.Location = new System.Drawing.Point(28, 29);
+            this.BtnOpenCSV.Name = "BtnOpenCSV";
+            this.BtnOpenCSV.Size = new System.Drawing.Size(75, 23);
+            this.BtnOpenCSV.TabIndex = 0;
+            this.BtnOpenCSV.Text = "Ouvrir CSV";
+            this.BtnOpenCSV.UseVisualStyleBackColor = true;
+            this.BtnOpenCSV.Click += new System.EventHandler(this.BtnOpenCSV_Click);
+            // 
+            // OvrirCSV
+            // 
+            this.OvrirCSV.FileName = "OfdCSV";
+            // 
+            // TxtBTes
+            // 
+            this.TxtBTes.Location = new System.Drawing.Point(274, 31);
+            this.TxtBTes.Multiline = true;
+            this.TxtBTes.Name = "TxtBTes";
+            this.TxtBTes.Size = new System.Drawing.Size(376, 382);
+            this.TxtBTes.TabIndex = 1;
+            this.TxtBTes.TextChanged += new System.EventHandler(this.TxtBTes_TextChanged);
+            // 
+            // EleveActivites
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.TxtBTes);
+            this.Controls.Add(this.BtnOpenCSV);
+            this.Name = "EleveActivites";
+            this.Text = "Eleves/activites";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnOpenCSV;
+        private System.Windows.Forms.OpenFileDialog OvrirCSV;
+        private System.Windows.Forms.TextBox TxtBTes;
     }
 }
 
--- eleves-activites/eleves-activites/Form1.cs
+++ eleves-activites/eleves-activites/Form1.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace eleves_activites
 {
-    public partial class Form1 : Form
+    public partial class EleveActivites : Form
     {
-        public Form1()
+        public EleveActivites()
         {
             InitializeComponent();
+        }
+
+        private void BtnOpenCSV_Click(object sender, EventArgs e)
+        {
+            string fileName = null;
+
+            using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
+            {
+                openFileDialog1.InitialDirectory = "c:\\";
+                openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog1.FilterIndex = 2;
+                openFileDialog1.RestoreDirectory = true;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    fileName = openFileDialog1.FileName;
+                }
+            }
+
+            if (fileName != null)
+            {
+                //Do something with the file, for example read text from it
+                string text = File.ReadAllText(fileName);
+                TxtBTes.Text += text;
+            }
+        }
+
+        private void TxtBTes_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
